0c1b912061c1dc114bbded453eeeee166049f804
Diego Haz <hazdiego@gmail.com>
2016-09-03 19:23:26 -0300

    Move Home from routes to components

--- src/routes/home.js
+++ src/routes/home.js
@@ -8,8 +8,8 @@
  */
 
 import React from 'react';
-import Home from './Home';
-import fetch from '../../core/fetch';
+import Home from '../components/Home';
+import fetch from '../core/fetch';
 
 export default {
 
